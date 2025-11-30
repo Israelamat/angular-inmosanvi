1da867e3a8ccd5fd955275be9e0a391b5b472bfb
Israelamat <israelamatgil@gmail.com>
2025-11-30 13:30:49 +0100

    properties-page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { PropertiesPage } from './properties-page/properties-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, PropertiesPage, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/properties-page/properties-page.ts
@@ -0,0 +1,70 @@
+import { ChangeDetectorRef, Component, inject} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Property } from '../interfaces/propoerty';
+
+@Component({
+  selector: 'properties-page',
+  imports: [FormsModule],
+  templateUrl: './properties-page.html',
+  styleUrl: './properties-page.css',
+})
+export class PropertiesPage {
+  filename: string = "";
+
+  properties: Property[] = [];
+  newProperty: Property = {
+    province: '',
+    town: '',
+    address: '',
+    title: '',
+    price: 0,
+    sqmeters: 0,
+    numRooms: 0,
+    numBaths: 0,
+    mainPhoto: '',
+  };
+
+  #changeDetector = inject(ChangeDetectorRef);
+
+  private resetProperty(){
+    this.newProperty = {
+      id:0,
+      province:"",
+      town:"",
+      address:"",
+      title:"",
+      price:0,
+      sqmeters:0,
+      numRooms:0,
+      numBaths:0,
+      mainPhoto:""
+    }
+    this.filename="";
+  }
+
+  changeImage(fileInput: HTMLInputElement) {
+    if (!fileInput.files || fileInput.files.length === 0) {
+      this.newProperty.mainPhoto = "";
+      return;
+    }
+
+    const reader:FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', () => {
+      this.newProperty.mainPhoto = reader.result as string;
+      this.#changeDetector.markForCheck(); 
+    });
+  }
+
+   addProperty() {
+    this.newProperty.id = Math.max(...this.properties.map(p=> p.id!))+1;
+
+    this.properties.push(this.newProperty);
+    this.filename = "";
+    this.resetProperty();
+   }
+
+  deleteProperty(id: number | undefined) {
+    this.properties = this.properties.filter(property => property.id !== id);
+  }
+}
